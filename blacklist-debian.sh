a895d054c2cf3a6ed34b991a6c5229d894fe1e80
Alexia <88803801+alexiarstein@users.noreply.github.com>
2023-01-03 02:01:57 -0300

    script para debian

---
+++ blacklist-debian.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# IP BLACKLISTER - v1.0
+# Author: Alexia Rivera
+# E-Mail: lachicadesistemas@gmail.com
+# Usage: bash blacklist.sh (cron)
+# LICENSE: GNU GPL 3.0
+# ------------------------------------
+
+# REQUIRERE FIREWALLD (sudo apt-get install firewalld)
+
+# settings
+# RHEL-CENTOS-FEDORA
+# modificar AUTH por /var/log/secure
+# si se desea generar listado de lo antiguo, hacer un for loop a /var/log/secure-*
+
+AUTH="/var/log/auth.log" 
+#--------------------------------------------------------------------------------------
+
+# correr por unica vez - Escanea los backups y obtiene las IPs.
+for i in /var/log/auth.log.*.gz; do zcat $i | grep "Failed password for invalid user" | awk '{print $13}' >> /tmp/tmp_auth_unsorted.log ; done
+for i in /var/log/auth.log.*.gz; do zcat $i | grep "Failed password for root from" | awk '{print $11}' >> /tmp/tmp_auth_unsorted.log; done
+grep "Failed password for invalid user" /var/log/auth.log.1 | awk '{print $13}' >> /tmp/tmp_auth_unsorted.log
+grep "Failed password for root from" /var/log/auth.log.1 | awk '{print $11}' >> /tmp/tmp_auth_unsorted.log
+#--------------------------------------------------------------
+
+
+# Primero generamos un log de lo que ya existe en /etc/firewalld/zones/public.xml
+grep "<source address=" /etc/firewalld/zones/public.xml | sed 's/"/,/g' | awk -F ',' '{print $2}'  > /tmp/tmp_zones_public.log
+
+# Luego generamos un nuevo listado con lo que haya en /var/log/auth.log
+grep "Failed password for invalid user" $AUTH | awk '{print $13}' >> /tmp/tmp_auth_unsorted.log
+grep "Failed password for root from" $AUTH | awk '{print $11}' >> /tmp/tmp_auth_unsorted.log
+
+# Eliminamos los duplicados, ya que una misma IP puede haber intentado pegarle al root u otro user inexistente.
+
+sort -u /tmp/tmp_auth_unsorted.log > /tmp/tmp_auth.log
+
+# Limpiamos /tmp/tmp_auth.log para que solo existan IPs en el log.
+# Y escribimos el archivo depurado en /tmp/tmp_firewall.log
+while IFS= read -r line
+do
+    # Check if the line is a valid IP address
+    if [[ $line =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] ; then
+        # If it is, print the IP address
+        echo "$line" >> /tmp/tmp_firewall.log
+    fi
+done < /tmp/tmp_auth.log
+
+# Ahora verificamos si las IPs que acabamos de generar no se encuentran ya blacklisteadas.
+
+while IFS= read -r line
+do
+    if ! grep -q "$line" /tmp/tmp_zones_public.log
+    then
+        echo "$line" >> /tmp/blacklist.log
+    fi
+done < /tmp/tmp_firewall.log
+
+rm /tmp/tmp_*
+# Por ultimo, solo le pasamos al firewall la lista sin repetidas.
+while read i; do
+echo -ne "Adding $i to blacklist: "
+firewall-cmd --permanent --add-rich-rule="rule family='ipv4' source address='$i' reject"; done < /tmp/blacklist.log
+# terminado el loopeo recargamos las reglas del FW.
+firewall-cmd --reload
